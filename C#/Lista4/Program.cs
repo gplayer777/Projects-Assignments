734c4a7d68ffb9cf73ceb39d54aa702ccd1149c2
gplayer777 <33601084+gplayer777@users.noreply.github.com>
2020-03-25 19:39:01 +0100

    Create Program.cs

---
+++ C#/Lista4/Program.cs
@@ -0,0 +1,29 @@
+namespace Projekt
+{
+class Program
+    {
+        static void Main(string[] args)
+        {
+        
+
+            RandomWordStream rs = new RandomWordStream();
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine(rs.next());
+
+
+			      PrimeCollection primes = new PrimeCollection();
+            primes.GetEnumerator().MoveNext();
+			         int k = 0;
+
+			      foreach (Prime p in primes)
+			      {
+				      Console.WriteLine(p.val);
+				      k++;
+				      if (k == 10) break;
+			      }
+
+
+
+		}
+    }
+    }
